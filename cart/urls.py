097b9e381660d16c776753f620f34cb63f58dbb8
hossein <hossein.h.a770227@gmail.com>
2024-03-01 04:38:24 -0800

    add add to cart form

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from .views import cart_create_view
+from .views import cart_create_view, add_to_cart_view
+
+
+app_name= 'cart'
 
 urlpatterns = [
     path('create/', cart_create_view, name='cart_create'),
+    path('add/<int:product_id>/', add_to_cart_view, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect 
+
 
 
 from .cart import Cart
+from products.models import Product
+from .forms import AddToProductForm
 
 
 def cart_create_view(request):
     cart=Cart(request)
     return render(request, 'cart/cart_create.html',{'cart':cart})
+
+
+def add_to_cart_view(request, product_id):
+    cart= Cart(request)
+    product= get_object_or_404(Product, id=product_id)
+
+    form= AddToProductForm(request.POST)
+
+    if form.is_valid():
+        cleaned_data= form.cleaned_data
+        quantity= cleaned_data['quantity']
+        cart.add(product, quantity)
+    return redirect('cart:cart_create')
