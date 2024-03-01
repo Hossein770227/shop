797e49eda6275b0ff3db732557e65d36197781b2
hossein <hossein.h.a770227@gmail.com>
2024-03-01 08:10:53 -0800

    add context processors cart

--- cart/urls.py
+++ cart/urls.py
@@ -8,5 +8,5 @@ app_name= 'cart'
 urlpatterns = [
     path('create/', cart_create_view, name='cart_create'),
     path('add/<int:product_id>/', add_to_cart_view, name='cart_add'),
-    path('remove/<int:product_id>/', remove_from_cart, name='cart_add'),
+    path('remove/<int:product_id>/', remove_from_cart, name='cart_remove'),
 ]
