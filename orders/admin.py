0b94c9afe58a68b858ba49d43f8632803ce0a911
hossein <hossein.h.a770227@gmail.com>
2024-03-04 04:19:18 -0800

    add order and orderitem model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user','first_name','last_name','phone','adress','is_paid']
+
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order','product','quantity','price',]
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete= models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+
+    first_name = models.CharField(max_length=100)
+    last_name =models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    adress = models.CharField(max_length=700)
+    order_notes = models.TextField()
+
+    date_time_created = models.DateTimeField(auto_now_add= True)
+    date_time_modified = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return f'order {self.id}'
+
+
+class OrderItem(models.Model):
+
+    order = models.ForeignKey(Order,  on_delete= models.CASCADE, related_name= 'items')
+    product = models.ForeignKey('products.Product', on_delete= models.CASCADE, related_name= 'orderitems')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'order{self.order.id} product:{self.product} quantity {self.quantity} with(price {self.price}) '
