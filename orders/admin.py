8039d4daa4f6bfd25af2f966ad6668d19d4cf2e0
hossein <hossein.h.a770227@gmail.com>
2024-03-08 01:05:57 -0800

    add tabulartinline

--- orders/admin.py
+++ orders/admin.py
@@ -2,12 +2,21 @@ from django.contrib import admin
 
 from .models import Order, OrderItem
 
+class OrderItemsInlines(admin.TabularInline):
+    model = OrderItem
+    fields = ['order','product','quantity','price',]
+    extra = 1
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['user','first_name','last_name','phone','adress','is_paid']
+    inlines = [
+        OrderItemsInlines,
+    ]
 
 
 
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['order','product','quantity','price',]
+# @admin.register(OrderItem)
+# class OrderItemAdmin(admin.ModelAdmin):
+#     list_display = ['order','product','quantity','price',]
--- products/admin.py
+++ products/admin.py
@@ -3,11 +3,23 @@ from django.contrib import admin
 
 from .models import Product, Comment
 
+
+class ProductCommentIlines(admin.TabularInline):
+    model = Comment
+    fields = ['user', 'product', 'text','active','stars']
+    extra = 1
+
+
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display=['title', 'price', 'date_time_created', 'active', ]
+    inlines = [
+        ProductCommentIlines,
+    ]
+    
 
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display=['user', 'product', 'date_time_created', 'active','stars']
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display=['user', 'product', 'date_time_created', 'active','stars']
